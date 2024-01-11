35ca70d6e18e3ea6fb1db53e600b857e1d3ae1ca
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2024-01-11 14:51:34 +0530

    Updated Solution - GfG to GitHub

--- Hard/Merge Without Extra Space/merge-without-extra-space.cpp
+++ Hard/Merge Without Extra Space/merge-without-extra-space.cpp
@@ -6,60 +6,23 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
     public:
-    
-        void swapElem(long long arr1[], long long arr2[], int index1, int index2){
-            if(arr1[index1]> arr2[index2]){
-                swap(arr1[index1], arr2[index2]);
-            }
-        }
         //Function to merge the arrays.
         void merge(long long arr1[], long long arr2[], int n, int m) 
         { 
             // code here 
-            // Approach 1: Using 2 pointers 
-            // int first = n-1;
-            // int second = 0;
-            // while(first>=0 && second<m){
-            //     if(arr1[first] > arr2[second]){
-            //         int temp = arr1[first];
-            //         arr1[first] = arr2[second];
-            //         arr2[second] = temp;
-            //         first--;
-            //         second++;
-            //     }else{
-            //         break;
-            //     }
-            // }
-            
-            // sort(arr1, arr1+n);
-            // sort(arr2, arr2+m);
-            
-            
-            // Approach 2: Using Gap Method -> Shell Sort
-            int len = n+m;
-            int gap = len/2 + len%2;
-            while(gap>0){
-               int left = 0;
-               int right = left + gap;
-               while(right<len){
-                   if(left<n && right>= n){
-                        // arr1 and arr2
-                        swapElem(arr1, arr2, left, right-n);
-                   }else if(left>=n){
-                       // arr2 and arr2
-                       swapElem(arr2,arr2, left-n, right-n);
-                   }else{
-                       // arr1 and arr1
-                       swapElem(arr1,arr1, left, right);
-                   }
-                   left++;
-                   right++;
-               }
-               if(gap == 1){
-                   break;
-               }
-               gap = gap/2 + gap%2;
+            int index1 = n-1;
+            int index2 = 0;
+            while(index1>= 0 && index2 < m){
+                if(arr1[index1] > arr2[index2]){
+                    swap(arr1[index1], arr2[index2]);
+                    index1--;
+                    index2++;
+                }else{
+                    break;
+                }
             }
+            sort(arr1, arr1+n);
+            sort(arr2, arr2+m);
         } 
 };
 
